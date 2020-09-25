801e8a5056baacbce58a8993a005030ac90e50b6
Iftikhor99 <1987-2005@mail.ru>
2020-09-25 22:37:33 +0500

    добавленна функция PeriodsDynamic

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -55,3 +55,37 @@ func CategoriesAvg(payments []types.Payment) map[types.Category]types.Money {
 
 	return categoriesAverage
 }
+
+func PeriodsDynamic(first map[types.Category]types.Money, second map[types.Category]types.Money) map[types.Category]types.Money{
+	result := map[types.Category]types.Money{}
+	mianKey := types.Category("")
+	
+	for key1 := range first {
+		for key2 := range second {
+			if key2 == key1 {
+				continue				
+			}		
+			second[key1] += types.Money(0)
+		}			
+	}
+
+	for key2 := range second {
+		for key1 := range first {
+			if key1 == key2 {
+				continue				
+			}		
+			first[key2] += types.Money(0)
+		}			
+	}
+
+	for key1, category1 := range first {
+		mianKey = key1
+		for key2, category2 := range second {
+			if mianKey == key2 {
+				result[mianKey] = category2 - category1
+			}		
+			continue
+		}			
+	}
+	return result
+}
